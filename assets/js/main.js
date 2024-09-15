e46292544bdb91d647f8df2f306b41b03019bcf8
K <kiet100.huynh@gmail.com>
2024-09-15 13:07:45 +0700

    1

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+   section.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height) {
+         navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+         })
+      }
+   })
+}
+
+
+menuIcon.addEventListener("click", () => {
+      if (navbar.style.display === "block") {
+           navbar.style.display = "none";
+      } else {
+           navbar.style.display = "block";
+      }
+     menuIcon.classList.toggle("bx-x");
+});
